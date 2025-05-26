df19f7154033c65e24716c5f3944da5c2406c822
steph <van.stepa@mail.ru>
2025-05-26 20:08:14 +0300

    initial commit

---
+++ src/main/java/ru/van/tg_web_app_auth/TgWebAppAuthApplication.java
@@ -0,0 +1,13 @@
+package ru.van.tg_web_app_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgWebAppAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgWebAppAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/van/tg_web_app_auth/TgWebAppAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.van.tg_web_app_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgWebAppAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
